16ef0165aa1cc8d20be4623a605c488468eb08a6
Naoufal hdr <naoufalhdr@gmail.com>
2023-10-27 20:43:05 +0100

    Task 11: Add a C program that creates 5 zombie processes.

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+/**
+ * infinite_while - run an infinit loop.
+ *
+ * Return: always 0
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - create a five zombie processes.
+ *
+ * Return: 0 if success.
+ */
+int main(void)
+{
+	pid_t child_pid;
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		child_pid = fork();
+		if (child_pid > 0)
+		{
+			printf("Zombie process created, PID: %d\n", child_pid);
+			sleep(1);
+		}
+		else
+			exit(0);
+	}
+
+	infinite_while();
+
+	return (0);
+}
