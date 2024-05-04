92daef2afbf178272eba657dfc25efa674843929
Naoufal hdr <naoufalhdr@gmail.com>
2024-05-04 18:04:02 +0100

    Task 4: Client configuration wiht puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+# Puppet Manifest for SSH Client Configuration
+
+file_line { "Turn off passwd auth":
+  path   => "~/.ssh/config",
+  line   => 'PasswordAuthentication no',
+  match  => '^PasswordAuthentication',
+  ensure =>  present,
+}
+
+file_line { "Declare identity file":
+  path   => "~/.ssh/config
+  line   => 'IdentityFile ~/.ssg/school',
+  match  => '^IdentityFile',
+  ensure => present,
+}
