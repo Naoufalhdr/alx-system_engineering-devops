52a2bc50bee389a0f302442a43143a2b759542cf
Naoufal hdr <naoufalhdr@gmail.com>
2024-05-04 18:09:33 +0100

    Task 4: fix a couple of syntax errors and typos

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,15 @@
 # Puppet Manifest for SSH Client Configuration
 
-file_line { "Turn off passwd auth":
-  path   => "~/.ssh/config",
+file_line { 'Turn off passwd auth':
+  path   => '~/.ssh/config',
   line   => 'PasswordAuthentication no',
   match  => '^\s*PasswordAuthentication',
   ensure =>  present,
 }
 
-file_line { "Declare identity file":
-  path   => "~/.ssh/config
-  line   => 'IdentityFile ~/.ssg/school',
+file_line { 'Declare identity file':
+  path   => '~/.ssh/config',
+  line   => 'IdentityFile ~/.ssh/school',
   match  => '^\s*IdentityFile',
   ensure => present,
 }
