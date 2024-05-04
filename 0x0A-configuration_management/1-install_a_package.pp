10c0fd8ee46db32974e4c3bd4319cf6303e4bc27
Naoufal hdr <naoufalhdr@gmail.com>
2024-05-04 10:04:23 +0100

    Task 1: fix a few syntax errors and inaccuracies.

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
-#!/usr/bin/pup
-# Define a package ressource to ensure Flask v2.1.0 is installed
-package {'flask':
+#!/usr/bin/puppet
+
+# Define a package resource to ensure Flask v2.1.0 is installed
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
 }
