8edcd0fd5abe0f30bcfd464d9ec474ea7b50833f
Naoufal hdr <naoufalhdr@gmail.com>
2024-05-07 15:26:41 +0100

    Task 5: test

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,35 +1,21 @@
 # Install Nginx and configuring my server with puppet
-include stdlib
 
-# Install Nginx package
 package { 'nginx':
   ensure => installed,
 }
 
-# Create the index html file
+file_line { 'install':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me youtube.com permanent;',
+}
+
 file { '/var/www/html/index.html':
-  ensure  => present,
-  content => "Hello World!",
+  content => 'Hello World!',
 }
 
-# Define Nginx configuration file
-file_line { 'ensure_line_exist':
-  ensure => present,
-  line   => 'listen 80 default_server;',
-  path   => '/etc/nginx/sites-available/default',
-}
-
-file_line { '301_redirection_insert':
-  ensure => present,
-  line   => '\n\tlocation /redirect_me {\n\t\treturn 301 youtube.com;\n\t}',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'server_name _;',
-}
-
-# Ensure Nginx service is running and enabled
 service { 'nginx':
   ensure  => running,
-  enable  => true,
   require => Package['nginx'],
 }
-
