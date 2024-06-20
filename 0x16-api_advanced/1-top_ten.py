9b9dead47ca37edac727320013424a25ab8f292f
Naoufalhdr <naoufalhdr96@gmail.com>
2024-06-20 21:59:17 +0100

    Task 1: trying fix check 4 and 5 errors

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,44 @@
 #!/usr/bin/python3
-"""Function to print hot posts on a given Reddit subreddit."""
+"""
+This module provides a function to query the Reddit API to fetch the hot
+posts for a given subreddit and print the titles of the first 10 posts
+"""
 import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    """
+    Queries the Reddit API and prints the titles of the first 10 hot posts
+    listed for a given subreddit. If the subreddit is invalid, prints None.
+    """
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
+            'User-Agent': (
+                'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:127.0) '
+                'Gecko/20100101 Firefox/127.0'
+                )
+            }
     params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
-        return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+            'limit': 9
+            }
+
+    try:
+        response = requests.get(url, headers=headers, params=params,
+                                allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            posts = data.get("data", {}).get("children", {})
+
+            if not posts:
+                print(None)
+                return
+
+            for post in posts:
+                print(f"{i} {post["data"]["title"]}")
+
+        else:
+            print(None)
+    except Exception as e:
+        print(e)
+        print(None)
