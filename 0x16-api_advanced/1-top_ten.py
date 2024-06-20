8130c65e244c9e28c5928198bf61a8d076e6f05f
Naoufalhdr <naoufalhdr96@gmail.com>
2024-06-20 22:22:46 +0100

    Task 1: test

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-""" This module provides a function to query the Reddit API to fetch the
-hot posts for a given subreddit and print the titles of the first 10 posts """
+"""
+This module provides a function to query the Reddit API to fetch the
+hot posts for a given subreddit and print the titles of the first 10 posts.
+"""
 import requests
 
 
@@ -12,29 +14,26 @@ def top_ten(subreddit):
 
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {
-            'User-Agent': (
-                'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:127.0) '
-                'Gecko/20100101 Firefox/127.0'
-                )
-            }
-    params = {
-            'limit': 9
-            }
+        'User-Agent': (
+            'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:127.0) '
+            'Gecko/20100101 Firefox/127.0'
+        )
+    }
+    params = {'limit': 10}
 
     try:
         response = requests.get(url, headers=headers, params=params,
                                 allow_redirects=False)
         if response.status_code == 200:
             data = response.json()
-            posts = data.get("data", {}).get("children", {})
+            posts = data.get("data", {}).get("children", [])
 
             if not posts:
                 print(None)
                 return
 
             for post in posts:
-                print(f"{post["data"]["title"]}")
-
+                print(post["data"]["title"])
         else:
             print(None)
     except Exception as e:
