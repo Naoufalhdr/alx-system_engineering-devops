46e74c5ba5429c6b356698f36144c3693191db16
Naoufalhdr <naoufalhdr96@gmail.com>
2024-06-27 18:36:53 +0100

    Task 1: fix check 3 error

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,8 +1,13 @@
 # 1-user_limit.pp
 # The script will modify '/etc/security/limits.conf' to increase the number of open files for the holberton user.
 
-exec { 'change-os-configuration-for-holberton-user':
-  command => 'echo "holberton soft nofile 4096\nholberton hard nofile 8192" >> /etc/security/limits.conf',
-  path    => '/usr/bin:/usr/sbin:/bin:/sbin',
-  unless  => 'grep -q "holberton" /etc/security/limits.conf',
+exec {'replace-1':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
+  before   => Exec['replace-2'],
+}
+
+exec {'replace-2':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
 }
