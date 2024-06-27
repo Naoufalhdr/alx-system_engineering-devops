f740959feb883eba992ac7b64823bd39fbdaa071
Naoufalhdr <naoufalhdr96@gmail.com>
2024-06-27 18:32:44 +0100

    Task 0: try fixing check 4 error

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,13 @@
 # 0-the_sky_is_the_limit_not.pp
 # Increases the worker_connections setting in the Nginx configuration file from 1024 to 4096.
 
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/worker_connections 1024/worker_connections 4096/" /etc/nginx/nginx.conf && service nginx restart',
-  path    => '/usr/bin:/usr/sbin:/bin:/sbin',
+exec {'fix--for-nginx':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
+}
+
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
